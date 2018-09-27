9d8288dc17e27df55737c9ed423aa5977f7b6cd4
Shubhambhandari008 <43613337+Shubhambhandari008@users.noreply.github.com>
2018-09-27 08:55:29 +0530

    Add files via upload

---
+++ PC_CONTROL_ROBOT/PC_CONTROL_ROBOT.ino
@@ -0,0 +1,50 @@
+void setup()
+{
+pinMode(10,OUTPUT);
+pinMode(11,OUTPUT);
+pinMode(12,OUTPUT);
+pinMode(13,OUTPUT);
+Serial.begin(9600);
+Serial.println("START");
+}
+
+void loop()
+{
+if(Serial.available()>0)
+{
+char c = Serial.read();
+if(c=='f' || c=='F'){                         //Forward
+Serial.println("FORWARD");
+digitalWrite(10,HIGH);
+digitalWrite(11,LOW);
+digitalWrite(12,HIGH);
+digitalWrite(13,LOW);
+}
+
+if(c=='B' || c=='b'){                         //Backward
+Serial.println("BACKWARD");
+
+digitalWrite(10,LOW);
+digitalWrite(11,HIGH);
+digitalWrite(12,LOW);
+digitalWrite(13,HIGH);
+}
+if(c=='L' || c=='l'){
+Serial.println("TURN LEFT");
+digitalWrite(10,LOW);
+digitalWrite(11,HIGH);
+digitalWrite(12,HIGH);
+digitalWrite(13,LOW);
+}
+if(c=='R' || c=='r'){
+Serial.println("TURN RIGHT");
+digitalWrite(10,HIGH);
+digitalWrite(11,LOW);
+digitalWrite(12,LOW);
+digitalWrite(13,HIGH); 
+}
+}
+}
+
+
+
